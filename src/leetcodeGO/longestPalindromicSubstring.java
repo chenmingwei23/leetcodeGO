d3b8df75d428c2008371acdf88d43fb42922b62b
RTX2060 <RTX2060@192.168.1.113>
2021-05-26 00:42:54 +1000

    insert #

--- src/leetcodeGO/longestPalindromicSubstring.java
+++ src/leetcodeGO/longestPalindromicSubstring.java
@@ -1,27 +1,47 @@
 	package leetcodeGO;
 	
 	import java.util.*;
+import java.util.stream.Collectors;
 	
 	public class longestPalindromicSubstring {
 	
 	    public static String longestPalindrome(String s) {
-	    	String max = s;
-	    	int len = s.length();
-	    	boolean[][] isPalindrome= new boolean[len][len];
-	    	for (int i = 0; i<len;i++) {
-	    		char [] temp;
-	    		for (int j = len - 1; j >= i;j -- ) {
-	    			if (s.charAt(j)!=s.charAt(i)) {
-	    				continue;
+	    	String max = "";
+	    	int maxLength = 0;
+	    	char [] sequence = new char[2 * s.length() - 1];
+	    	for (int i = 0; i <s.length(); i ++) {
+	    		sequence[2*i] = s.charAt(i);
+	    		if (2*i+1 >= 2 * s.length() - 1) {
+	    			break;
+	    		}
+	    		sequence[2*i+1] = '#';
+	    	}
+	    	
+	    	for (int i = 0; i < 2*s.length() - 1; i ++ ) {
+	    		int forward = i;
+	    		int backward = i;
+	    		ArrayList<Character> temp = new ArrayList<>();
+	    		System.out.print(i+" "+sequence[i]+" ");
+	    		while (forward >0 && backward < 2*s.length() - 2) {
+	    			if (sequence[forward] != sequence[backward]) {
+	    				break;
 	    			}
-	    			temp = s.substring(i, j+1).toCharArray(); 
+	    			forward --;
+    				backward ++;
+	    			
+	    		}
+	    		System.out.println(forward + " " + backward+" ");
+	    		if(backward - forward > maxLength) {
+	    			maxLength = backward - forward;
+	    			max = s.substring(forward/2, backward/2 + 1);
 	    		}
 	    	}
+	    	
 	    	return max;
 	    }
 	    
 		public static void main(String args[]) {
-			String s = "ab";
+			String s = "babad";
 			System.out.println(longestPalindrome(s));
 		}
 	}
