4ea3b5293bd9758699541d7931a3a218d1f1d1d0
RTX2060 <RTX2060@192.168.1.113>
2021-05-27 01:47:42 +1000

    dfs判断法，完成

--- src/leetcodeGO/lexicalOrder.java
+++ src/leetcodeGO/lexicalOrder.java
@@ -3,10 +3,43 @@ package leetcodeGO;
 import java.util.*;
 
 public class lexicalOrder {
-	public List<Integer> lexicalOrder(int n) {
+	static void dfs(int n, int parent, List<Integer> result) {	
+		int now = parent * 10;
+		for (int i  = 0; i < 10; i++) {
+			if (now + i > n) {
+				return;
+			}
+			else {
+				System.out.println(now+i);
+				result.add(now+i);
+				dfs(n, now + i, result);	
+			}
+		}
+		return;
+		
+	}
+	static public List<Integer> lexicalOrder(int n) {
 		
 		
 		List<Integer> result = new ArrayList<>();
+		for (int i = 1; i < 10; i++) {
+			if (i > n) {
+				break;
+			}else {
+				System.out.println(i);
+				result.add(i);
+				dfs(n,i, result);
+			}
+		}
+
 		return result;
     }
+	
+	public static void main(String args[]) {
+		int n = 111;
+		List<Integer> res = lexicalOrder(n);
+		for (int i : res) {
+			System.out.println(i);
+		}
+	}
 }	
